1ff406fb301c693e29cef2dc7579728b277fe2dc
VedantYadav025 <136351429+VedantYadav025@users.noreply.github.com>
2024-01-21 13:08:45 +0530

    Create Domino piling.cpp

---
+++ Domino piling.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int m, n;
+  cin >> m >> n;
+  cout << (int)(m * n / 2) << "\n";
+  return 0;
+}
